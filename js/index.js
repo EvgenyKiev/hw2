be6ec89c8520e631ce607da760d8945ea7ffcace
Feliciter <padakluev@gmx.com>
2018-07-12 20:11:15 +0300

    commit

--- js/index.js
+++ js/index.js
@@ -33,10 +33,7 @@ function getRandomInt(min, max) {
 
     return t;
 }(document, "script", "fjs"));
-/////
 
-
-///
 
 function openURL(url){
         window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
@@ -44,43 +41,33 @@ function openURL(url){
 
     var apisrc="https://talaikis.com/api/quotes/random/";
     var f_getjson=function(json) {
-
     $(".quote").html( JSON.stringify(json.quote));
     $(".author").html( '<i>'+JSON.stringify(json.author)+'<i>');
-    }
-
+    };
 
     $(document).ready(function() {$.getJSON(apisrc,f_getjson);
             var currentQuote = $.getJSON(apisrc,f_getjson);
             var currentAuthor = $.getJSON(apisrc,f_getjson);
         $(".btn").on("click", function() {
             $("body").css('background', 'url("' + imageUrl[getRandomInt(0, imageUrl.length)] + '")', "#ffe").css('background-size', 'cover')
-        })
+        });
 
         $("#getMessage").on("click", function(){
             $.getJSON(apisrc,f_getjson);
 
-        })
-
-
-//
-//        $('#get_tweet').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + quote + '" ' + author));
-//
-
+        });
 
         $('#get_tweet').on('click', function() {
              {
                  var QQuote= document.getElementsByClassName('quote')["0"].innerText;
-                 console.log( QQuote)
+                //  console.log( QQuote)
 
                  var AAuthor= document.getElementsByClassName('author')["0"].innerText;
-                 console.log( AAuthor)
-
-
+                //  console.log( AAuthor)
 
                 openURL('https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + QQuote+' '+AAuthor );
 
             }
         });
 
-    })
+    });
